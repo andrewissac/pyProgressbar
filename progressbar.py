8480ed5516924f17d45bf61e7ee23d2f2f8346cb
Andrew <26877481+andrewissac@users.noreply.github.com>
2020-12-07 00:55:46 +0100

    added rainbow waves and some parameters

--- progressbar.py
+++ progressbar.py
@@ -36,28 +36,41 @@ class bcolors:
 def rgbToBash(r,g,b):
     return '\033[38;2;' + str(r) + ';' + str(g) + ';' + str(b) + 'm'
 
-def progress(count, total, cycleCharsDeque, progressbarSegments = 60, boldChars = False, message='', colorChangeSpeedParam = 1.0): 
+def hlsToBash(h,l,s):
+    r,g,b = tuple(int(x * 255) for x in c.hls_to_rgb(h, l, s))
+    return '\033[38;2;' + str(r) + ';' + str(g) + ';' + str(b) + 'm'
+
+def progress(
+    count, total, cycleCharsDeque, message='', 
+    progressbarSegments=60, boldChars=False, rainbowWave=True, 
+    charsColorDistanceParam=25, colorChangeSpeedParam=1.0,
+    luminosity=0.65, saturation=1.0): 
     # progressbarSegments % len(waveCharsDeque) should be 0 to ensure a perfect loop
     progressbarSegments = progressbarSegments # number of bar segments
     filledProgressbarSegments = int(round(progressbarSegments * count / float(total))) # number of bar segments that are filled
-    h = 0.0
-
+    # using HLS colorspace
+    h = 0.0 
     if (count / float(total)) * colorChangeSpeedParam < 1.0:
         h = (count / float(total)) * colorChangeSpeedParam
     else:
-        h = (count / float(total)) * colorChangeSpeedParam - 1.0 # make it repeat (remember h interval 0..1)
-    l = 0.6
-    s = 1.0
-    r,g,b = tuple(int(x * 255) for x in c.hls_to_rgb(h, l,s ))
+        h = (count / float(total)) * colorChangeSpeedParam - 1.0 # make it repeat (remember hue interval: 0..1)
+    l = luminosity # should be between 0 and 1
+    s = saturation # should be between 0 and 1
 
     percentage = round(100.0 * count / float(total), 1)
-    bar = rgbToBash(r,g,b)
+    bar = hlsToBash(h,l,s)
     if boldChars:
         bar += bcolors.BOLD
-    for i in range(filledProgressbarSegments):
-        bar += cycleCharsDeque[i]
-    bar += '-' * (progressbarSegments - filledProgressbarSegments)
+    if rainbowWave:
+        h_increment = (charsColorDistanceParam / float(total)) * colorChangeSpeedParam
+        for i in range(filledProgressbarSegments):
+            bar += cycleCharsDeque[i] + hlsToBash(h,l,s)
+            h += h_increment
+    else:
+        for i in range(filledProgressbarSegments):
+            bar += cycleCharsDeque[i]
     bar += bcolors.ENDC
+    bar += '-' * (progressbarSegments - filledProgressbarSegments)
 
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percentage, '%', message))
     sys.stdout.flush()
@@ -77,15 +90,20 @@ def repeatList(l, n):
 # total = 1500 # could be any number depending on your problem
 # wave = ',.~*^`^*~.'
 # tableflip = '(ノಠ 益ಠ)ノ彡┻━┻........'
-# chars = [char for char in wave]
+# rectangles = '▮▮▮▮▮▮▮▮▮▮'
+# chars = [char for char in tableflip]
 
 # progressbarSegments = 60 # 
 # cycleCharsDeque = deque(repeatList(chars, progressbarSegments)) # deque needed for cyclic rotation of chars
 
 # for i in range(total):
 #     sleep(0.02) # simulate task that takes a while
+#     progress(
+#         count=i, total=total, cycleCharsDeque=cycleCharsDeque, message='myMessage',
+#         progressbarSegments=progressbarSegments, boldChars=False, rainbowWave=True, 
+#         charsColorDistanceParam=25, colorChangeSpeedParam=4.0,
+#         luminosity=0.65, saturation=1.0)
 
-#     progress(count=i, total=total, cycleCharsDeque=cycleCharsDeque, boldChars=False, progressbarSegments=progressbarSegments, message='LUUUL', colorChangeSpeedParam=4.0)
 #     if(i % 5 == 0): # simple mechanism to only rotate chars every 5 loop steps (may vary in your case)
-#         cycleCharsDeque.rotate(-1) # cyclic rotation of charsList
+#         cycleCharsDeque.rotate(1) # cyclic rotation of charsList
 # endregion 
